4c5f17ba66b39baedb58280d62e0b45265f477bf
Filipe Abelha <me@filipeabelha.com>
2017-06-07 09:56:57 -0300

    Updated some URI solutions

--- uri/1708.cpp
+++ uri/1708.cpp
@@ -1,17 +1,35 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9+7, EPS = 1e-6;
+
+int x, y, xx, yy, ans;
 
 int main () {
-    int lap;
-    int fastest, slowest;
-    scanf("%d%d", &fastest, &slowest);
-    for (int i = 1; i < 10000; i++) {
-        int f = fastest * i;
-        int s = slowest * i;
-        if (s - f >= fastest) {
-            lap = i + 1;
-            break;
-        }
+    scanf("%d%d", &x, &y);
+    while (1) {
+        xx += x;
+        yy += y;
+        ans++;
+        if (yy-xx >= x) break;
     }
-    printf("%d\n", lap);
+    printf("%d\n", ans+1);
     return 0;
 }
--- uri/1709.cpp
+++ uri/1709.cpp
@@ -1,15 +1,39 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main () {
-    int n;
-    scanf("%d", &n);
-    int pos = 2;
-    int count = 1;
-    while (pos != 1) {
-        if (pos > n/2) pos = n - (n - pos)*2 - 1; else pos = pos*2;
-        count++;
-    }
-    printf("%d\n", count);
-    return 0;
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9+7, EPS = 1e-6, N = 2e5+10;
+
+int v[N], ans;
+
+void go (int x) {
+    ans++;
+    if (x == 1) return;
+    go(v[x]);
 }
 
+int main () {
+    int n; scanf("%d", &n);
+    int h = n/2;
+    for (int i = 1; i <= h; i++) v[i] = 2*i;
+    for (int i = h+1; i <= n; i++) v[i] = v[i-h]-1;
+    go(2);
+    printf("%d\n", ans);
+    return 0;
+}
