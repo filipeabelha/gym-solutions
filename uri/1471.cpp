33a85fec6096b739240740484ea15dca48b3eb9a
Filipe Abelha <me@filipeabelha.com>
2017-06-19 22:56:03 -0300

    Updated URI 1471

--- uri/1471.cpp
+++ uri/1471.cpp
@@ -1,21 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9+7, EPS = 1e-6, N = 1e4+10;
+
+int n, r, x, m[N], ok;
+
 int main () {
-    int n, r;
-    while(scanf("%d%d", &n, &r) != EOF) {
-        vector <int> vol;
-        vol.push_back(0);
-        for (int i = 0; i < n; i++) vol.push_back(1);
-        for (int i = 0; i < r; i++) {
-            int k;
-            scanf("%d", &k);
-            vol[k] = 0;
-        }
-        int noprint = 1;
-        for (int i = 0; i < vol.size(); i++)
-            if (vol[i]) printf("%d ", i), noprint = 0;
-        if (noprint) printf("*\n"); else printf("\n");
+    while (~scanf("%d%d", &n, &r)) {
+        cl(m, 0); ok = 0;
+        for (int i = 1; i <= r; i++)
+            scanf("%d", &x), m[x] = 1;
+        for (int i = 1; i <= n; i++)
+            if (!m[i]) printf("%d ", i), ok = 1;
+        printf("%s\n", ok ? "" : "*");
     }
     return 0;
 }
