04c622bbd4905e2388b8898c8f9341a7dc5114ce
Filipe Abelha <me@filipeabelha.com>
2017-06-28 01:34:26 -0300

    Added Codeforces 812C (Bin Search)

---
+++ cdf/812c.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9+7, EPS = 1e-6, N = 1e5+10;
+
+ll n, s, v[N], l, r, m, cost, add;
+
+int main () {
+    scanf("%lld%lld", &n, &s);
+    for (int i = 0; i < n; i++) scanf("%lld", &v[i]);
+    l = 0, r = n;
+    while (l <= r) {
+        m = (l+r+1)/2; cost = 0;
+        priority_queue <ll> pq;
+        for (int i = 1; i <= n; i++) add = m*i+v[i-1], pq.push(-add);
+        for (int i = 0; i < m; i++) cost += -pq.top(), pq.pop();
+        if (cost > s) r = m-1; else l = m;
+        if (cost == s or m == r) break;
+    }
+    printf("%lld %lld\n", m, cost);
+    return 0;
+}
