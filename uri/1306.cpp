6897f53a9ec09b4019af8c264188c6a839dafc6f
Filipe Abelha <me@filipeabelha.com>
2017-07-15 03:42:46 -0300

    Added URI 1306

---
+++ uri/1306.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int r, n, cnt;
+
+int main () {
+    while (~scanf("%d%d", &r, &n) and r and n and ++cnt) {
+        printf("Case %d: ", cnt);
+        int ans = r/n - !(r%n);
+        (ans > 26 ? puts("impossible") : printf("%d\n", ans));
+    }
+    return 0;
+}
