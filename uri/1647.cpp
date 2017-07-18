e15077f5108be64fe6476e3283ef2055a4bc5432
Filipe Abelha <me@filipeabelha.com>
2017-07-18 04:57:35 -0300

    Added URI 1647

--- uri/1647.cpp
+++ uri/1647.cpp
@@ -22,13 +22,13 @@ const ld EPS = 1e-9, PI = acos(-1.);
 const int INF = 0x3f3f3f3f, MOD = 1e9+7;
 const int N = 1e2+5;
 
-int n, v[N];
-ll ans;
+int n;
+ll v[N], ans;
 
 int main () {
     while (~scanf("%d", &n) and n) {
         ans = 0;
-        for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+        for (int i = 0; i < n; i++) scanf("%lld", &v[i]);
         for (int i = n-1; i >= 0; i--) {
             for (int j = i-1; j >= 0; j--) v[j] += v[i];
             ans += v[i];
