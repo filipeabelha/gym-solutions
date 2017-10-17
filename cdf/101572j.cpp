3653fabb7ac9c33c32026dddcc220a94d521909b
Filipe Abelha <me@filipeabelha.com>
2017-10-17 09:32:56 -0200

    Added Codeforces 101572J

---
+++ cdf/101572j.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int a, b;
+
+int main () {
+    scanf("%d%d", &a, &b);
+    if (a and a == b) printf("Even %d\n", a+b);
+    else if (a == b) printf("Not a moose\n");
+    else printf("Odd %d\n", 2*max(a, b));
+    return 0;
+}
