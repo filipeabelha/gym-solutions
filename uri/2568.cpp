ceaf5f9f26e670b3aeb9769bf2277a25ad1304f9
Filipe Abelha <me@filipeabelha.com>
2017-10-20 09:39:25 -0200

    Added URI 2568

---
+++ uri/2568.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int a, b, c, d;
+
+int main () {
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    for (int i = a+1; i <= a+d; i++)
+        if (i&1) b -= c; else b += c;
+    printf("%d\n", b);
+    return 0;
+}
