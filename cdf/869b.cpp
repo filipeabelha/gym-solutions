4c7bc5cf747886d2229fe76bd66965568155076f
Filipe Abelha <me@filipeabelha.com>
2017-11-03 01:58:20 -0200

    Added Codeforces 869B

---
+++ cdf/869b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+ll a, b;
+int ans = 1;
+
+int main () {
+    scanf("%lld%lld", &a, &b);
+    for (ll i = b; i > a; i--) {
+        ans = (ans*(i%10))%10;
+        if (!ans) break;
+    }
+    printf("%d\n", ans);
+    return 0;
+}
