7ac828d05464b87310183415df1a6730e82a1011
Filipe Abelha <me@filipeabelha.com>
2018-03-19 02:56:58 -0300

    Added UVa 11849

---
+++ uva/11849.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n, m, x, ans;
+set <int> s;
+
+int main () {
+    while (~scanf("%d%d", &n, &m) and (n or m)) {
+        s.clear();
+        ans = 0;
+        while (n--) scanf("%d", &x), s.insert(x);
+        while (m--) scanf("%d", &x), ans += s.count(x);
+        printf("%d\n", ans);
+    }
+    return 0;
+}
