9c087e655a3891272e9af7e02af2cbdbc4919441
Filipe Abelha <me@filipeabelha.com>
2018-03-23 22:58:40 -0300

    Added UVa 11235

---
+++ uva/11235.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n, q, x, y, v[N], c[N][2], st[4*N];
+
+void build (int p, int l, int r) {
+    if (l == r) {st[p] = 1; return;}
+    int m = (l+r)/2;
+    build(2*p,   l,   m);
+    build(2*p+1, m+1, r);
+    st[p] = max(st[2*p], st[2*p+1]);
+    if (v[m] == v[m+1]) {
+        int a = (v[l] != v[m]   ? c[m][0]   : c[m][0]   - c[l][0] + 1);
+        int b = (v[r] != v[m+1] ? c[m+1][1] : c[m+1][1] - c[r][1] + 1);
+        st[p] = max(st[p], a+b);
+    }
+}
+
+int query (int p, int l, int r, int i, int j) {
+    if (r < i or j < l) return 0;
+    if (i <= l and r <= j) return st[p];
+    int m = (l+r)/2;
+    int q1 = query(2*p,   l,   m, i, j);
+    int q2 = query(2*p+1, m+1, r, i, j);
+    int ans = max(q1, q2);
+    if (i <= m and m+1 <= j and v[m] == v[m+1]) {
+        int a = (v[i] != v[m]   ? c[m][0]   : c[m][0]   - c[i][0] + 1);
+        int b = (v[j] != v[m+1] ? c[m+1][1] : c[m+1][1] - c[j][1] + 1);
+        ans = max(ans, a+b);
+    }
+    return ans;
+}
+
+int main () {
+    while (~scanf("%d", &n) and n) {
+        cl(v, 0);
+        cl(c, 0);
+        cl(st, 0);
+
+        scanf("%d", &q);
+        for (int i = 1; i <= n; i++) scanf("%d", &v[i]);
+
+        c[1][0] = c[n][1] = 1;
+        for (int i = 2; i <= n; i++)
+            c[i][0] = (v[i] == v[i-1] ? c[i-1][0] + 1 : 1);
+        for (int i = n-1; i >= 1; i--)
+            c[i][1] = (v[i] == v[i+1] ? c[i+1][1] + 1 : 1);
+
+        build(1, 1, n);
+        while (q--) {
+            scanf("%d%d", &x, &y);
+            printf("%d\n", query(1, 1, n, x, y));
+        }
+    }
+    return 0;
+}
