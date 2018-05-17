818953864bf81784f36e00fdd7f98679672151ff
Filipe Abelha <me@filipeabelha.com>
2018-05-17 17:00:26 -0300

    Added Codeforces 982A

---
+++ cdf/982a.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n, ok = 1, cnt;
+string s;
+
+int main () {
+    cin >> n >> s;
+    int sz = s.size();
+
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] == '1' and s[i-1] == '1') ok = 0;
+    }
+
+    for (int i = 1; i < s.size()-1; i++) {
+        if (s[i] == '0' and (s[i-1] == '0' and s[i+1] == '0')) ok = 0;
+    }
+
+    if (s.size() > 1) {
+        if (s[0] == '0' and s[1] == '0') ok = 0;
+        if (s[sz-1] == '0' and s[sz-2] == '0') ok = 0;
+    } else {
+        ok = (s[0] == '1');
+    }
+
+    printf("%s\n", ok ? "Yes" : "No");
+    return 0;
+}
