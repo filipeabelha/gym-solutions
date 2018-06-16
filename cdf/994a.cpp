83e46e39375ca3c9f61acdca72c126cafdf59a0f
Filipe Abelha <me@filipeabelha.com>
2018-06-16 18:08:47 -0300

    Added Codeforces 994A

---
+++ cdf/994a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n, m, x, a[N];
+set <int> s;
+
+int main () {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 1; i <= m; i++) {
+        scanf("%d", &x);
+        s.insert(x);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (s.count(a[i])) printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
