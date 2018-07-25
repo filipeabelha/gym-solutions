61326078d491b4596a921dc36a731d9eccc8e95c
Filipe Abelha <me@filipeabelha.com>
2018-07-25 11:19:55 -0300

    Added URI 2748

---
+++ uri/2748.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int main () {
+    for (int i = 1; i <= 39; i++) printf("-"); printf("\n");
+    printf("|");       for (int i = 1; i <= 8;  i++) printf(" ");
+    printf("Roberto"); for (int i = 1; i <= 22; i++) printf(" "); printf("|\n");
+
+    printf("|"); for (int i = 1; i <= 37; i++) printf(" "); printf("|\n");
+
+    printf("|");       for (int i = 1; i <= 8;  i++) printf(" ");
+    printf("5786");    for (int i = 1; i <= 25; i++) printf(" "); printf("|\n");
+
+    printf("|"); for (int i = 1; i <= 37; i++) printf(" "); printf("|\n");
+
+    printf("|");       for (int i = 1; i <= 8;  i++) printf(" ");
+    printf("UNIFEI");  for (int i = 1; i <= 23; i++) printf(" "); printf("|\n");
+
+    for (int i = 1; i <= 39; i++) printf("-"); printf("\n");
+    return 0;
+}
