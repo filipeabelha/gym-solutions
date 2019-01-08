58e464ff6d7fb6decd67f2d2b271e1fc45a42d77
Filipe Abelha <me@filipeabelha.com>
2019-01-08 15:13:11 -0200

    Added URI 2807

---
+++ uri/2807.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+ll f[50];
+int n;
+
+int main () {
+    f[1] = f[2] = 1;
+    for (int i = 3; i < 45; i++) f[i] = f[i-1] + f[i-2];
+    scanf("%d", &n);
+    for (int i = n; i >= 1; i--) printf("%lld%c", f[i], " \n"[i==1]);
+    return 0;
+}
