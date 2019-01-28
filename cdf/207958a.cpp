dbe7e68850f16278ce4364826ff8cc0d9b41e0e3
Filipe Abelha <me@filipeabelha.com>
2019-01-28 20:49:58 -0200

    Added Codeforces 207958A (UFBA group; union-find with rollback)

---
+++ cdf/207958a.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n, q, o, a, b;
+int par[N], sz[N];
+stack <pii> sp, ss;
+
+int find (int x) { return par[x] == x ? x : find(par[x]); }
+
+void unite (int a, int b) {
+    if ((a = find(a)) == (b = find(b))) return;
+    if (sz[a] > sz[b]) swap(a, b);
+    sp.push({a, par[a]});
+    ss.push({b, sz[b]});
+    par[a] = b;
+    sz[b] += sz[a];
+}
+
+int main () {
+    for (int i = 0; i < N; i++) par[i] = i, sz[i] = 1;
+    scanf("%d%d", &n, &q);
+    while (q--) {
+        scanf("%d", &o);
+        if (o == 3) {
+            par[sp.top().st] = sp.top().nd; sp.pop();
+            sz[ss.top().st]  = ss.top().nd; ss.pop();
+        } else {
+            scanf("%d%d", &a, &b);
+            if (o == 2) unite(a, b);
+            else printf("%d\n", find(a) == find(b));
+        }
+    }
+    return 0;
+}
