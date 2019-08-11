b3f362d262567862c42291efae71b5a198c6a827
Filipe Abelha <me@filipeabelha.com>
2019-08-11 00:04:39 -0300

    Added Codeforces 100625I

---
+++ cdf/100625i.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int tc, n;
+string s, t;
+map <string, int> f;
+
+int main () {
+    cin >> tc;
+    while (tc--) {
+        cin >> n;
+        while (n--) {
+            cin >> s >> t;
+            f[t]++;
+        }
+        ll ans = 1;
+        for (auto p : f) ans *= (p.nd+1);
+        ans--;
+        cout << ans << endl;
+        f.clear();
+    }
+    return 0;
+}
