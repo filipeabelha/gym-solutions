de6cb58e4de19d690e47c25468de2e8a07ee6af3
Filipe Abelha <me@filipeabelha.com>
2019-08-20 15:07:56 -0300

    Added UVa 455 (Periodic Strings)

---
+++ uva/455.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+string s, p, q;
+
+vi z (string q) {
+    vi z(q.size());
+    for (int i = 1, l = 0, r = 0, n = q.size(); i < n; i++) {
+        if (i <= r) z[i] = min(z[i-l], r-i+1);
+        while (i + z[i] < n and q[z[i]] == q[z[i] + i]) z[i]++;
+        if (i+z[i]-1 > r) {
+            l = i;
+            r = i+z[i]-1;
+        }
+    }
+    return z;
+}
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> s;
+        string ans = {s[0]};
+        int n = s.size();
+        vi v = z(s);
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0 and i + v[i] == n) break;
+            ans += s[i];
+        }
+        cout << ans.size() << endl;
+        if (t) cout << endl;
+    }
+
+    return 0;
+}
