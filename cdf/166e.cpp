cd101b80fc08155cea39dfc11f862f207be36cba
Filipe Abelha <me@filipeabelha.com>
2019-10-10 18:59:54 -0300

    Added Codeforces 166E

---
+++ cdf/166e.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e7+5;
+
+ll dp[N];
+
+int main () {
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i < N; i++) dp[i] = (2*dp[i-1]+3*dp[i-2]) % MOD;
+
+    ll n;
+    scanf("%lld", &n);
+    printf("%lld\n", (3*dp[n-1]) % MOD);
+
+    return 0;
+}
