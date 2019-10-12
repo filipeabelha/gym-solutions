d061a4c8318d15577fbf6ad7e5ee2be2f9a7aba8
Filipe Abelha <me@filipeabelha.com>
2019-10-12 12:16:43 -0300

    Added Codeforces 765C

---
+++ cdf/765c.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+ll k, a, b, ans;
+
+ll solve (ll k, ll a, ll b) {
+    ll A = a/k;
+    ll B = b/k;
+    ll ans = A + B;
+    if (a % k != 0 and B == 0) ans = -1;
+    if (b % k != 0 and A == 0) ans = -1;
+    return ans;
+}
+
+int main () {
+    scanf("%lld%lld%lld", &k, &a, &b);
+    printf("%lld\n", solve(k, a, b));
+    return 0;
+}
