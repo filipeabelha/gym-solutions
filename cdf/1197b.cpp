c14ab36d456650080a7ba584aad3bb7a66ffb431
Filipe Abelha <me@filipeabelha.com>
2019-10-31 15:08:54 -0300

    Added Codeforces 1197B

---
+++ cdf/1197b.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 2e5+5;
+
+int n, mx, ind;
+
+int main () {
+    scanf("%d", &n);
+    vi v(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+        if (mx < v[i]) {
+            mx = v[i];
+            ind = i;
+        }
+    }
+    int ok = 1;
+    for (int i = 1; i <= ind; i++)
+        if (v[i-1] > v[i]) ok = 0;
+    for (int i = ind+1; i < n; i++)
+        if (v[i-1] < v[i]) ok = 0;
+
+    printf("%s\n", ok ? "YES" : "NO");
+    return 0;
+}
