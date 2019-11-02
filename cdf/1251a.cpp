cbdfaa54f73c7eeda2408d0c2ff02b3c4d9e3c84
Filipe Abelha <me@filipeabelha.com>
2019-11-02 11:12:02 -0300

    Added Codeforces 1251A

---
+++ cdf/1251a.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int t;
+string s;
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> t;
+    while (t--) {
+        cin >> s;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int cur = 0;
+            int ok = 0;
+            for (auto d : s) {
+                if (d == c) cur++;
+                else {
+                    if (cur&1) ok = 1;
+                    cur = 0;
+                }
+            }
+            if (cur&1) ok = 1;
+            if (ok) cout << c;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
