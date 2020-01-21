22c6e84e23034d774f2d265cdc6d23cedb8cda8a
Filipe Abelha <me@filipeabelha.com>
2020-01-21 20:20:49 -0300

    Added Codeforces 388C

---
+++ cdf/388c.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n, s, a, b, x;
+vi v;
+
+int main () {
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%d", &s);
+        for (int i = 0; i < s/2; i++) {
+            scanf("%d", &x);
+            a += x;
+        }
+        if (s&1) {
+            scanf("%d", &x);
+            v.pb(x);
+        }
+        for (int i = 0; i < s/2; i++) {
+            scanf("%d", &x);
+            b += x;
+        }
+    }
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++) {
+        if (i&1) b += v[i];
+        else a += v[i];
+    }
+    printf("%d %d\n", a, b);
+
+    return 0;
+}
