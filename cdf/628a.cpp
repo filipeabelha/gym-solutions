0b4792d18040b8f023c3d3741ca187b221ea7bd1
Filipe Abelha <me@filipeabelha.com>
2020-03-06 16:48:56 -0300

    Added Codeforces 628A

---
+++ cdf/628a.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937_64 llrand(random_device{}());
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+#define gcd(x, y) __gcd((x), (y))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f, LMOD = 1011112131415161719ll;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+ll n, b, p, x, y;
+
+int main () {
+    scanf("%lld%lld%lld", &n, &b, &p);
+    y = n*p;
+    while (n > 1) {
+        ll i = (1 << 20);
+        while (i > n) i >>= 1;
+        x += (2*b+1)*(i/2);
+        n -= i/2;
+    }
+
+    printf("%lld %lld\n", x, y);
+
+
+    return 0;
+}
