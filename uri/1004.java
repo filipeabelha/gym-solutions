9084d083d6c00040c8cef33c0f02bd2f67d2e8ff
Filipe Abelha <me@filipeabelha.com>
2020-06-06 17:43:39 -0300

    Adding 1004.java

---
+++ uri/1004.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    public static void main (String[] args) throws IOException {
+
+        MyScanner sc = new MyScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int a, b, p;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        p = a*b;
+        out.printf("PROD = %d\n", p);
+
+        out.close();
+    }
+
+    public static PrintWriter out;
+
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+}
